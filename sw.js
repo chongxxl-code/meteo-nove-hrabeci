0c81b6e8603d1083fdaee4d2d39602a90e122345
chongxxl-code <chong.xxl@gmail.com>
2026-08-20 18:02:46 +0200

    Restore dedicated OneSignal worker setup

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-const CACHE='nh-meteo-shell-v06';
+const CACHE='nh-meteo-shell-v07';
 const SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
